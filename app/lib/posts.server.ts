ec82666c41ae0fe067aa99566ad3b1d94811a5df
Martin Belev <martinbelev93@gmail.com>
2022-08-03 09:27:50 +0300

    switch from vercel to netlify build target
    - switch from using npm to yarn - netlify needs a different package for esbuild in order to work. Yarn is supporting this by defining supported os architectures.
    - move images from posts in public directory for now (need to rethink how to better do this)
    - include mdx files into netlify functions so that the files can be read from the filesystem

--- app/lib/posts.server.ts
+++ app/lib/posts.server.ts
@@ -5,10 +5,7 @@ import type { FrontMatter, Post, StaticPath } from '../types/Post';
 import getCompiledMDX from './prepare-mdx.server';
 import readingTime from 'reading-time';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-`${__dirname}/../app/posts`;
-
-const postsDirectory = path.join(__dirname, '..', 'app/posts');
+const postsDirectory = path.join(__dirname, '../..', 'app/posts');
 
 interface FileName {
   fullFileName: string;
--- app/lib/prepare-mdx.server.ts
+++ app/lib/prepare-mdx.server.ts
@@ -1,24 +1,6 @@
 import { bundleMDX } from 'mdx-bundler';
-import path from 'path';
 
 const getCompiledMDX = async (source: string, directory: string) => {
-  if (process.platform === 'win32') {
-    process.env.ESBUILD_BINARY_PATH = path.join(
-      process.cwd(),
-      'node_modules',
-      'esbuild',
-      'esbuild.exe'
-    );
-  } else {
-    process.env.ESBUILD_BINARY_PATH = path.join(
-      process.cwd(),
-      'node_modules',
-      'esbuild',
-      'bin',
-      'esbuild'
-    );
-  }
-
   const [{ default: rehypePrism }, { default: remarkMdxImages }] = await Promise.all([
     // @ts-ignore
     import('rehype-prism-plus'),
@@ -32,21 +14,6 @@ const getCompiledMDX = async (source: string, directory: string) => {
     xdmOptions: (options) => {
       options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMdxImages];
       options.rehypePlugins = [...(options.rehypePlugins ?? []), rehypePrism];
-
-      return options;
-    },
-    esbuildOptions: (options) => {
-      options.outdir = path.join(process.cwd(), 'public', 'img', 'posts');
-      options.loader = {
-        ...options.loader,
-        '.png': 'file',
-        '.jpg': 'file',
-        '.jpeg': 'file'
-      };
-      options.publicPath = '/img/posts';
-
-      // Set write to true so that esbuild will output the files.
-      options.write = true;
 
       return options;
     }
