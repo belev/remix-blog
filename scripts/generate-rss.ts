ddb6b122992c893f339cee8a6658498420444b3c
Martin Belev <martinbelev93@gmail.com>
2022-08-09 18:24:10 +0300

    simplify rss generation by reusing already generated posts content

--- scripts/generate-rss.ts
+++ scripts/generate-rss.ts
@@ -1,27 +1,9 @@
 import * as fs from 'fs/promises';
-import path from 'path';
-import matter from 'gray-matter';
-import { globby } from 'globby';
 import RSS from 'rss';
 import siteConfig from '../siteConfig.json';
-import type { FrontMatter, Post } from '~/types/Post';
+import content from '../app/content.json';
 
 (async () => {
-  const postPaths = await globby(['app/posts/*.mdx']);
-  const readPosts = postPaths.map(async (postPath) => {
-    const fullPath = path.join(process.cwd(), postPath);
-    const fileContents = await fs.readFile(fullPath, 'utf8');
-
-    const matterResult = matter(fileContents);
-
-    return {
-      slug: postPath.replace('app/posts/', 'blog/').replace(/\.mdx$/, ''),
-      ...(matterResult.data as FrontMatter)
-    };
-  });
-
-  const postsData: Post[] = await Promise.all(readPosts);
-
   const rss = new RSS({
     title: siteConfig.siteTitle,
     description: siteConfig.siteDescription,
@@ -30,12 +12,12 @@ import type { FrontMatter, Post } from '~/types/Post';
     language: 'en'
   });
 
-  postsData.forEach(({ title, description, date, slug }) => {
+  Object.values(content.posts).forEach(({ title, description, date, slug }) => {
     rss.item({
       title,
       description,
       date,
-      url: `${siteConfig.siteUrl}/${slug}`
+      url: `${siteConfig.siteUrl}/blog/${slug}`
     });
   });
 
