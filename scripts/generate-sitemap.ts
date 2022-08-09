fad88415bd11dfff3d220d5fef1fdb2776eda4e3
Martin Belev <martinbelev93@gmail.com>
2022-08-09 08:31:26 +0300

    generate tags in sitemap.xml

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -2,11 +2,21 @@ import * as fs from 'fs/promises';
 import { globby } from 'globby';
 import prettier from 'prettier';
 import siteConfig from '../siteConfig.json';
+import content from '../app/content.json';
+
+const getXmlUrlItem = (route: string): string => {
+  return `
+    <url>
+      <loc>${`${siteConfig.siteUrl}${route}`}</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.7</priority>
+    </url>
+  `;
+};
 
 (async () => {
   const pages = await globby([
     'app/routes/*.tsx',
-    'app/posts/*.mdx',
     '!app/routes/blog.$slug.tsx',
     '!app/routes/tags.$slug.tsx'
   ]);
@@ -16,20 +26,22 @@ import siteConfig from '../siteConfig.json';
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
         ${pages
           .map((page) => {
-            const path = page
-              .replace('app/routes', '')
-              .replace('app/posts', '/blog')
-              .replace('.tsx', '')
-              .replace('.mdx', '');
+            const path = page.replace('app/routes', '').replace('.tsx', '');
             const route = path === '/index' ? '' : path;
 
-            return `
-              <url>
-                  <loc>${`${siteConfig.siteUrl}${route}`}</loc>
-                  <changefreq>daily</changefreq>
-                  <priority>0.7</priority>
-              </url>
-            `;
+            return getXmlUrlItem(route);
+          })
+          .join('')}
+
+        ${Object.keys(content.posts)
+          .map((postSlug) => {
+            return getXmlUrlItem(`/blog/${postSlug}`);
+          })
+          .join('')}
+
+        ${Object.keys(content.tags)
+          .map((tagSlug) => {
+            return getXmlUrlItem(`/tags/${tagSlug}`);
           })
           .join('')}
     </urlset>
